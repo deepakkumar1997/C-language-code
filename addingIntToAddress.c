8092d63643778b60c50acbc7b9cd328a2feb2a0a
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2021-06-09 02:03:36 +0530

    Added .vscode in gitigonre

--- addingIntToAddress.c
+++ addingIntToAddress.c
@@ -6,10 +6,7 @@ int main()
     printf("Address of a : %d\n",p);
     //I'm adding 5 to address of var a but in address of 5*4=20 will be added because in this
     // gcc compiler Integer data are of 4 bytes so why it is 5*4=20(bytes)
-<<<<<<< HEAD
-=======
     //In this address I'm going to add 5 but it will add 5*sizeof(5)=5*4=20.
->>>>>>> addNewFeature
     printf("Adding int-5 to address of a : a+5 = %d\n",p+5);
     return 0;
 }
