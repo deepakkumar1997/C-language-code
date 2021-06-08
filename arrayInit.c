3057a9c39358c93c1d70cc3cb1d1b326526b58c9
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2021-06-09 04:13:14 +0530

    Different types of array initialization

---
+++ arrayInit.c
@@ -0,0 +1,10 @@
+//An array can be partially initialized, as in: int arr[5] = { 1, [4]{5}, [2]{3} }; 
+//after that, the value of: arr[0] is 1, arr[1] is 0 arr[2] is 3 arr[3] is 0 arr[4] is 5
+#include<stdio.h>
+int main()
+{
+    int arr[5] = {1, [4]{5},[2]{3}};
+    for(int i=0;i<5;i++)
+    printf("Array[%d] = %d\n",i,arr[i]);
+    return 0;
+}
