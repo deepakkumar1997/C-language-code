35809f3a2bc785fe14f9317c2fa7e78eec63689d
deepakkumar1997 <pandeydeepak.kumar1997@gmail.com>
2021-06-08 17:50:59 +0530

    Added a comment

--- addingIntToAddress.c
+++ addingIntToAddress.c
@@ -6,6 +6,7 @@ int main()
     printf("Address of a : %d\n",p);
     //I'm adding 5 to address of var a but in address of 5*4=20 will be added because in this
     // gcc compiler Integer data are of 4 bytes so why it is 5*4=20(bytes)
+    //hello
     printf("Adding int-5 to address of a : a+5 = %d\n",p+5);
     return 0;
 }
